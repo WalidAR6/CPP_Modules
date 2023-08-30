d1d1fd5c121bd0bf408ecd635e74e20b8958b97c
Walid Araissi <waraissi@e3r10p6.1337.ma>
2023-08-30 02:27:15 +0100

    day 03 ex00 almost done

---
+++ Cpp_03/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/30 00:43:05 by waraissi          #+#    #+#             */
+/*   Updated: 2023/08/30 02:25:32 by waraissi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int main()
+{
+	ClapTrap a("walid");
+	a.attack("to");
+	a.attack("to");
+	a.attack("to");
+	a.attack("to");
+	a.attack("to");
+	a.attack("to");
+	a.attack("to");
+	a.attack("to");
+	a.attack("to");
+	a.attack("to");
+	a.attack("to");
+	a.attack("to");
+}
