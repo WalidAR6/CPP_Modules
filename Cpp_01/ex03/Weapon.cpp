f9b0c023899a538e9ea4bb40d1b0438a4e2cc93e
Walid Araissi <waraissi@e3r3p18.1337.ma>
2023-09-13 17:36:01 +0100

    fix naming function

--- Cpp_01/ex03/Weapon.cpp
+++ Cpp_01/ex03/Weapon.cpp
@@ -6,7 +6,7 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 16:10:37 by waraissi          #+#    #+#             */
-/*   Updated: 2023/09/12 21:39:33 by waraissi         ###   ########.fr       */
+/*   Updated: 2023/09/13 11:39:53 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ std::string	*Weapon::get_type()
 	return (&type);
 }
 
-std::string const & Weapon::getType() const
+const std::string & Weapon::getType() const
 {
 	return (type);
 }
