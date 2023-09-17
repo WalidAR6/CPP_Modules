4f10d127dff9bc060d2d46d20a3d62503c07773c
Walid Araissi <waraissi@e3r4p10.1337.ma>
2023-09-17 06:25:45 +0100

    fix bugs

--- Cpp_03/ex00/ClapTrap.hpp
+++ Cpp_03/ex00/ClapTrap.hpp
@@ -6,37 +6,37 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 00:43:03 by waraissi          #+#    #+#             */
-/*   Updated: 2023/09/17 00:12:11 by waraissi         ###   ########.fr       */
+/*   Updated: 2023/09/17 06:19:02 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAP_TRAP_H
-# define CLAP_TRAP_H
+#define CLAP_TRAP_H
 
 #include <iostream>
 
-class ClapTrap
-{
-	private:
-		std::string name;
-		int hitPoint;
-		int energyPoint;
-		int attackDamage;
-	public:
-		ClapTrap();
-		ClapTrap(std::string name);
-		ClapTrap(const ClapTrap & obj);
-		ClapTrap & operator=(const ClapTrap & obj);
-		~ClapTrap();
+class ClapTrap {
+private:
+  std::string name;
+  int hitPoint;
+  int energyPoint;
+  int attackDamage;
 
-		std::string getName() const;
-		int getHitPoint() const;
-		int getEnergyPoint() const;
-		int getAttackDamage() const;
+public:
+  ClapTrap();
+  ClapTrap(std::string name);
+  ClapTrap(const ClapTrap &obj);
+  ClapTrap &operator=(const ClapTrap &obj);
+  ~ClapTrap();
 
-		void attack(const std::string & target);
-		void takeDamage(unsigned int amout);
-		void beRepaired(unsigned int amout);
+  std::string getName() const;
+  int getHitPoint() const;
+  int getEnergyPoint() const;
+  int getAttackDamage() const;
+
+  void attack(const std::string &target);
+  void takeDamage(unsigned int amout);
+  void beRepaired(unsigned int amout);
 };
 
 #endif
