e3e15a1ba6d90e841bca16752d4d3b182612d397
Walid Araissi <waraissi@e2r6p7.1337.ma>
2023-11-26 00:43:22 +0100

    i should provide more tests in the last 07 exercise

--- Cpp_07/ex02/Array.hpp
+++ Cpp_07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 02:23:37 by waraissi          #+#    #+#             */
-/*   Updated: 2023/11/25 19:21:11 by waraissi         ###   ########.fr       */
+/*   Updated: 2023/11/26 00:42:23 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,8 @@ template<typename T> class Array
         Array &operator=(const Array & obj)
         {
             if (this == &obj)
-                return *this;
-            if(array)
-                delete [] array;
+                return *this;    
+            delete [] array;
             array = new T[obj._size];
             _size = obj._size;
             for (size_t idx = 0; idx < obj._size; idx++)
@@ -56,7 +55,7 @@ template<typename T> class Array
         T &operator[](size_t idx)
         {
             if (idx > size() || idx < 0)
-                throw std::runtime_error("out of bound");
+                throw std::runtime_error("OUT OF RANGE!!!");
             return array[idx];
         }
         
