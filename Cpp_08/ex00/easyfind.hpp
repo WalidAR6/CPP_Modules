8267aa55344570c883f8cc9247d6b73dbd121512
Walid Araissi <waraissi@e2r6p7.1337.ma>
2023-11-30 00:00:04 +0100

    modifying iteration methods in the fnuction

--- Cpp_08/ex00/easyfind.hpp
+++ Cpp_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 20:15:18 by waraissi          #+#    #+#             */
-/*   Updated: 2023/11/27 23:22:23 by waraissi         ###   ########.fr       */
+/*   Updated: 2023/11/29 17:23:23 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,12 @@
 template<typename T>
 void easyfind(T container, int num)
 {
-    for (typename T::iterator idx = container.begin(); idx != container.end(); idx++)
-    {
-        if (*idx == num)
-        {
-            std::cout << "Found it, the value is: " << *idx << std::endl;
-            return ;
-        }
-    }
-    std::cout << "Element not found" << std::endl;
+    typename T::iterator it;
+    it = std::find(container.begin(), container.end(), num);
+    if (it != container.end())
+        std::cout << "Found it, element is: " << *it << std::endl;
+    else
+        std::cout << "Element not found" << std::endl;
 }
 
 #endif
